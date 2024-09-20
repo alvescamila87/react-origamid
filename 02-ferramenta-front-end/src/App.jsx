9965e4df4aaabeacd79ad6ef0f7ea35b288a43d4
Camila Alves <alvescamila87@gmail.com>
2024-09-20 17:45:37 -0300

    JSX 1

--- 02-ferramenta-front-end/src/App.jsx
+++ 02-ferramenta-front-end/src/App.jsx
@@ -1,11 +1,39 @@
 import React from "react"
 
+const titulo = <h1>Título da página</h1>
+
 const App = () => {
 
+  const nome = "Camila"
+  const ativo = false;
+
+  function mostrarNome(sobrenome) {
+    return "Marlene " + sobrenome
+  }
+
+  const carro = {
+    marca: 'VW',
+    modelo: 'Golf',
+    rodas: '4'
+  }
+
+  const estiloP = {
+    color: 'blue',
+    fontSize: "2rem",
+  }
+
   return (
-    <div>
-      App Reacts <button>Adicionar</button>
-    </div>
+    <>     
+      {titulo}
+      {mostrarNome('Theodora')}
+      <p>{true ? 'Ativo' : 'Inativo'} - { 10 }</p>
+      <p>{new Date().getFullYear()}</p>
+      <p style={estiloP}>Informações do carro (objeto)</p>
+      <p style={estiloP}>{carro.marca}</p>
+      <p style={estiloP}>{carro.modelo}</p>
+      <p style={estiloP}>{carro.rodas}</p>
+      <p className={ativo ? 'ativo' : 'inativo'}>{nome}</p>
+    </>
   )
 }
 
