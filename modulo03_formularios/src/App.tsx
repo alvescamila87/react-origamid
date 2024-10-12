e6365c2ac7bece21e2a6490b44a46fe52de6480e
Camila Alves <alvescamila87@gmail.com>
2024-10-12 17:54:49 -0300

    textArea

--- modulo03_formularios/src/App.tsx
+++ modulo03_formularios/src/App.tsx
@@ -1,18 +1,22 @@
 import { Formulario01 } from "./Form/formulario01"
 import { Formulario02 } from "./Form/formulario02"
 import { Formulario03 } from "./Form/formulario03"
+import TextArea from "./Form/textArea"
 
 function App() {
 
 
   return (
     <>
-      {/* <h1>Hello App</h1>
+      <h1>Hello App</h1>
       <h2>Form 01</h2>
       <Formulario01 />
       <h2>Form 02</h2>
-      <Formulario02 /> */}
+      <Formulario02 />
+      <h2>Form 03</h2>
       <Formulario03 />
+      <h2>Form com TextArea</h2>
+      <TextArea />
     </>
   )
 }
---
+++ modulo03_formularios/src/Form/textArea.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const TextArea = () => {
+  const [textArea, setTextArea] = useState("");
+
+  return (
+    <form>
+      <textarea 
+        value={textArea} 
+        onChange={(event) => setTextArea(event.target.value)}
+        rows="5"
+      />
+    </form>
+  );
+};
+
+export default TextArea;
