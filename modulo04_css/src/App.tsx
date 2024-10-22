4d1bb691fe49244bff4597d83037729045526c25
Camila <alvescamila87@gmail.com>
2024-10-22 20:36:17 -0300

    css modules

--- modulo04_css/src/App.tsx
+++ modulo04_css/src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css';
 import { Title } from './cssImport/Title';
+import { Produto } from './cssModules/components/Produto';
 
 function App() {
   return (
-    <div className="container">
-      <Title />
-    </div>
+    <>
+      <h3>css Import</h3>
+      <div className="container">
+        <Title />
+      </div>
+      <h3>css Modules</h3>
+      <Produto />
+    </>
 
   )
 }
